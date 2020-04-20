7a4c5c7c9819bbb03fb20f439e44653e3e09ac09
John Osorio <john.osorio@bsc.es>
2020-04-20 13:50:06 +0200

    New eval_params_several_weights.py file to run a new set of validation tests

---
+++ keras/evaluation_tests/bash_eval_one_plot_several_weights.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python eval_params_several_weights.py --weights_train \
+    /gpfs/scratch/bsc28/bsc28459/3dgan_data/weights_fp32_test_xscale_fix/generator_params_generator_epoch_029.hdf5 \
+    /gpfs/scratch/bsc28/bsc28459/3dgan_data/weights_mp_test_xscale_fix/generator_params_generator_epoch_029.hdf5 \
+    /gpfs/scratch/bsc28/bsc28459/3dgan_data/weights_auto_test_xscale_fixed/generator_params_generator_epoch_029.hdf5 \
+    --real_data /gpfs/scratch/bsc28/bsc28459/3dgan_data/EleEscan_1_9.h5 \
+    --out_dir ./output_test_eval_params_last/ \
+    --xscales 100 100 100 \
+    --labels fp32 mp auto
